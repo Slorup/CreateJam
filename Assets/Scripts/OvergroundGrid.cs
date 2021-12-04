e43843805f8bf1de0ab7981a3a6e1eadb46d18de
Odum <kristianodum@gmail.com>
2021-12-04 22:41:21 +0100

    rema and c4

--- Assets/Scripts/OvergroundGrid.cs
+++ Assets/Scripts/OvergroundGrid.cs
@@ -1,17 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class OvergroundGrid : MonoBehaviour
 {
     public int width = 32;
     public int height = 128;
     public int startHeight = 20;
-    public float cloudChance = 0.7f;
+    public float cloudChance = 1f;
     public float doubleCloudChance = 0.3f;
     public float cloud1Chance = 0.2f;
-    public float cloud2Chance = 0.6f;
-    public float cloud3Chance = 1.0f;
+    public float cloud2Chance = 0.4f;
+    public float cloud3Chance = 0.7f;
+    public float cloud4Chance = 1.0f;
     public GameObject cloud1;
     public GameObject cloud2;
     public GameObject cloud3;
@@ -37,12 +40,24 @@ public class OvergroundGrid : MonoBehaviour
             Quaternion.identity);
         Instantiate(rema, new Vector3(firstRemaLocation, startHeight - 4f),
             Quaternion.identity);
-        
+
+        int lastRema = 25;
         for (int y = startHeight; y < height; y++)
         {
             if (Random.value < cloudChance)
             {
-                int x = Mathf.RoundToInt(Random.Range(0f, (float)width));
+                if (y - lastRema > 25)
+                {
+                    int xx = Mathf.RoundToInt(Random.Range(5f, width-5f));
+                    Instantiate(rema, new Vector3(xx, y+1), Quaternion.identity);
+                    Instantiate(cloud4, new Vector3(xx, y), Quaternion.identity);
+                    y += 3;
+                    lastRema = y;
+                    continue;
+                }
+                
+                
+                int x = Mathf.RoundToInt(Random.Range(2f, (float)width));
                 float cloudSpecifier = Random.value;
                 if (cloudSpecifier < cloud1Chance)
                 {
@@ -54,16 +69,19 @@ public class OvergroundGrid : MonoBehaviour
                 }
                 else if (cloudSpecifier < cloud3Chance && x + 2 <= width)
                 {
-                        Instantiate(cloud3, new Vector3(x, y), Quaternion.identity);
-                        //Instantiate(rema, new Vector3(x, y + 1.3f), Quaternion.identity);
+                    Instantiate(cloud3, new Vector3(x, y), Quaternion.identity);
+                }
+                else if (cloudSpecifier < cloud4Chance && x + 3 <= width)
+                {
+                    Instantiate(cloud4, new Vector3(x, y), Quaternion.identity);
                 }
                 
 
-                y += 3;
+                y += 2;
             }
         }
 
-        for (int x = 2; x < width; x += 4)
+        for (int x = 6; x < width; x += 4)
         {
             GameObject c = Instantiate(cloudTop, new Vector3(x-0.5f, height + 1), Quaternion.identity);
             if (Random.value < 0.5)
