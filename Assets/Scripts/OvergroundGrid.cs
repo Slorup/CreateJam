520e63a3d00f24961bb1cc0b8235d4c5b6edbc5a
Odum <kristianodum@gmail.com>
2021-12-04 15:11:35 +0100

    Top cloud

--- Assets/Scripts/OvergroundGrid.cs
+++ Assets/Scripts/OvergroundGrid.cs
@@ -6,42 +6,56 @@ public class OvergroundGrid : MonoBehaviour
 {
     public int width = 32;
     public int height = 128;
-    public int startHeight = 20;
-    public float cloudChance = 2f/32f;
-    public float cloud1Chance = 0.33f;
-    public float cloud2Chance = 0.66f;
+    public int startHeight = 10;
+    public float cloudChance = 0.7f;
+    public float doubleCloudChance = 0.3f;
+    public float cloud1Chance = 0.2f;
+    public float cloud2Chance = 0.6f;
     public float cloud3Chance = 1.0f;
     public GameObject cloud1;
     public GameObject cloud2;
     public GameObject cloud3;
-    
-    
+    public GameObject cloudTop;
+    public GameObject pillar;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        Instantiate(pillar, new Vector3(20,1), Quaternion.identity);
+        Instantiate(pillar, new Vector3(21,1), Quaternion.identity);
+        Instantiate(pillar, new Vector3(21,2), Quaternion.identity);
+        
         for (int y = startHeight; y < height; y++)
         {
-            for (int x = 0; x < width; x++)
+            if (Random.value < cloudChance)
             {
-                if (Random.value < cloudChance)
+                int x = Mathf.RoundToInt(Random.Range(0f, (float)width));
+                float cloudSpecifier = Random.value;
+                if (cloudSpecifier < cloud1Chance)
                 {
-                    float cloudSpecifier = Random.value;
-                    if (cloudSpecifier < cloud1Chance)
-                    {
-                        Instantiate(cloud1, new Vector3(x, y, 0), Quaternion.identity);
-                    }
-                    else if (cloudSpecifier < cloud2Chance)
-                    {
-                        Instantiate(cloud2, new Vector3(x, y, 0), Quaternion.identity);
-                    }
-                    else if (cloudSpecifier < cloud3Chance)
-                    {
-                        Instantiate(cloud3, new Vector3(x, y, 0), Quaternion.identity);
-                    }
-
-                    x += 5;
-                    y += 2;
+                    Instantiate(cloud1, new Vector3(x, y), Quaternion.identity);
                 }
+                else if (cloudSpecifier < cloud2Chance && x + 1 <= width)
+                {
+                    Instantiate(cloud2, new Vector3(x, y), Quaternion.identity);
+                }
+                else if (cloudSpecifier < cloud3Chance && x + 2 <= width)
+                {
+                        Instantiate(cloud3, new Vector3(x, y), Quaternion.identity);
+                }
+
+                y += 3;
+            }
+        }
+
+        for (int x = 2; x < width; x += 4)
+        {
+            GameObject c = Instantiate(cloudTop, new Vector3(x-0.5f, height + 1), Quaternion.identity);
+            if (Random.value < 0.5)
+            {
+                c.GetComponent<SpriteRenderer>().flipX = true;
+                
             }
         }
     }
